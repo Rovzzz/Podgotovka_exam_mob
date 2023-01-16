d0e9413c682c1f61595c28aec5864b35cfbc79ef
zlobinovr@gmail.com <zlobinovr@gmail.com>
2023-01-16 21:56:30 +0300

    First commit

---
+++ app/src/main/java/com/example/exam_podg/Game.java
@@ -0,0 +1,23 @@
+package com.example.exam_podg;
+
+public class Game {
+    int id;
+    String title;
+    int cost;
+    int stockAvailability;
+    int availabilityInTheStore;
+    String description;
+    String rewiews;
+    String image;
+
+    public Game(int id, String title, int cost, int stockAvailability, int availabilityInTheStore, String description, String rewiews, String image) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+        this.stockAvailability = stockAvailability;
+        this.availabilityInTheStore = availabilityInTheStore;
+        this.description = description;
+        this.rewiews = rewiews;
+        this.image = image;
+    }
+}
